9d04869ee5cc9576ecd0b352766442b425ec76ec
Jogindar <jogindar@thesagenext.com>
2026-02-09 11:35:13 +0530

    version update

--- next.config.mjs
+++ next.config.mjs
@@ -134,11 +134,6 @@ const nextConfig = {
 		cssChunking: true,
 	},
 
-	webpack(config, { isServer }) {
-		if (!isServer) config.resolve.fallback = { fs: false, path: false };
-		return config;
-	},
-
 	async headers() {
 		const headers = [];
 
